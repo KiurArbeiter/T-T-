c54940f1c23dc0a5f3421386030c92ea5a23e9f2
KiurArbeiter <127192996+KiurArbeiter@users.noreply.github.com>
2023-11-07 10:50:12 +0200

    Add files via upload

---
+++ Töö/src/data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "jhgkasjdgy",
+      name: "ProductOne",
+      price: 50,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "ImgLink",
+    },
+    {
+      id: "jshdfhsp",
+      name: "ProductTwo",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "ImgLink",
+    },
+    {
+      id: "sjdfodis",
+      name: "ProductThree",
+      price: 150,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "ImgLink",
+    },
+    {
+      id: "läöakosad",
+      name: "ProductFour",
+      price: 200,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "ImgLink",
+    },
+  ];
